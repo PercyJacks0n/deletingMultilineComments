a139d09890c581bf78ddfc350d40ef63b9117336
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-07 23:13:47 +0300

    feat: add ErrorType enum

---
+++ ErrorType.h
@@ -0,0 +1,15 @@
+#ifndef ERRORTYPE_H
+#define ERRORTYPE_H
+
+enum ErrorType
+{
+    invalidArgumentsCount,
+    inputFileNotExist,
+    outputFileCreateError,
+    defineDirectiveFound,
+    numberRowsExceeded,
+    numberCharactersPerLineExceeded,
+    invalidFileExtension
+};
+
+#endif // ERRORTYPE_H

3d822719a9e87e90eaffac1d5bd379da8d1d6d71
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-07 23:20:33 +0300

    feat: add LocationFlagsInCode enum

---
+++ LocationsFlagsInCode.h
@@ -0,0 +1,13 @@
+#ifndef LOCATIONFLAGSINCODE_H
+#define LOCATIONFLAGSINCODE_H
+
+enum LocationFlagsInCode
+{
+    flagOuterCode,
+    flagMultiLineComment,
+    flagOneLineComment,
+    flagStringConstant,
+    flagSymbolicConstant
+};
+
+#endif // LOCATIONFLAGSINCODE_H

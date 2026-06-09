a3572ff522efa463054d6343d2820c034db27aa0
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-09 20:06:41 +0300

    refactor: rename LocationsFlagsInCode to LocationFlagInCode

---
+++ LocationFlagInCode.h
@@ -0,0 +1,13 @@
+#ifndef LOCATIONFLAGINCODE_H
+#define LOCATIONFLAGINCODE_H
+
+enum LocationFlagInCode
+{
+    flagOuterCode,
+    flagMultiLineComment,
+    flagOneLineComment,
+    flagStringConstant,
+    flagSymbolicConstant
+};
+
+#endif // LOCATIONFLAGINCODE_H

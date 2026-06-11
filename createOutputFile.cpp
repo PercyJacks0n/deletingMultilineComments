b1102d18059739d67f05df03262757b97d3e15bf
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-11 16:15:14 +0300

    stub: add createOutputFile stub

---
+++ createOutputFile.cpp
@@ -0,0 +1,12 @@
+#include <QTextStream>
+#include <QString>
+#include <QList>
+#include "Functions.h"
+#include "Error.h"
+#include "ErrorType.h"
+#include <QFile>
+
+void createOutputFile(const QString & content, const QString & filePath,QList<Error> & errors)
+{
+
+}

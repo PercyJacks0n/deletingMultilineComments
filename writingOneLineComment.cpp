8bbd0e2a5d075a3f2f3d0d47d23ed4c43d47c2d2
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-11 16:43:02 +0300

    stub: add writingOneLineComment stub

---
+++ writingOneLineComment.cpp
@@ -0,0 +1,8 @@
+#include <QString>
+#include "Functions.h"
+#include "LocationFlagInCode.h"
+
+void writingOneLineComment(const QString & inputCode, int & i, QString & result, LocationFlagsInCode & state)
+{
+
+}

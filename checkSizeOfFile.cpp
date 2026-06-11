17145352f1fed29e8ec88c55522c63ca536b4ca0
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-11 16:04:17 +0300

    stub: add checkSizeOfFile stub

---
+++ checkSizeOfFile.cpp
@@ -0,0 +1,10 @@
+#include <QString>
+#include <QList>
+#include "Functions.h"
+#include "Error.h"
+#include "ErrorType.h"
+
+void checkSizeOfFile(const QString & inputCode, QList<Error> & errors)
+{
+
+}

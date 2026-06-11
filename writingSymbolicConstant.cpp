e203327412f9786061accea61de0e42ff2807b4e
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-11 16:50:52 +0300

    stub: add writingSymbolicConstant stub

---
+++ writingSymbolicConstant.cpp
@@ -0,0 +1,9 @@
+#include <QString>
+#include "Functions.h"
+#include "LocationFlagInCode.h"
+
+void writingSymbolicConstant(const QString & inputCode, int & i, QString & result,LocationFlagInCode & state)
+{
+
+}
+

d90bfb2d7f6a8aeca1d725a72be5f54fb2aac7ff
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-11 15:55:46 +0300

    stub: add checkExtension stub

---
+++ checkExtension.cpp
@@ -0,0 +1,10 @@
+#include <QString>
+#include <QList>
+#include <QFileInfo>
+#include "Functions.h"
+#include "Error.h"
+
+void checkExtension(const QString & filePath, QList<QString> & allowedExtensions, QList<Error> & errors)
+{
+
+}

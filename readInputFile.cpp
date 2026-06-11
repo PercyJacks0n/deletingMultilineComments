7d483bb90d80776a0e8e93f9286539a02300b2d2
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-11 15:59:19 +0300

    stub: add readInputFile stub

---
+++ readInputFile.cpp
@@ -0,0 +1,13 @@
+#include <QString>
+#include <QList>
+#include <QFile>
+#include <QFileInfo>
+#include <QTextStream>
+#include "Functions.h"
+#include "Error.h"
+#include "ErrorType.h"
+
+QString readInputFile(const QString & filePath, QList<Error> & errors)
+{
+    return "";
+}

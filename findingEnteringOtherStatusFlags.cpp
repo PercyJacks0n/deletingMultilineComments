d5f8268f93a92689bdf11ea7213011b4804531b1
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-11 16:32:26 +0300

    stub: add findingEnteringOtherStatusFlags stub

---
+++ findingEnteringOtherStatusFlags.cpp
@@ -0,0 +1,13 @@
+#include <QString>
+#include "Functions.h"
+#include "LocationFlagInCode.h"
+
+void findingEnteringOtherStatusFlags(const QString & inputCode,
+                                     int & i,
+                                     QString & result,
+                                     LocationFlagsInCode & state,
+                                     int & countDirective,
+                                     QString & directive)
+{
+
+}

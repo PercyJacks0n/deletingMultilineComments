868c25b14ffe85b91dff6d021310a1a03506922c
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-12 18:16:00 +0300

    feat: implement isQuoteEscaped - green

--- isQuoteEscaped.cpp
+++ isQuoteEscaped.cpp
@@ -1,7 +1,12 @@
 #include <QString>
 #include "Functions.h"
 
-bool isQuoteEscaped(const QString & code, int pos)
+bool isQuoteEscaped(const QString &code, int pos)
 {
-    return true;
+    int backslashCount = 0;
+    for (int j = pos - 1; j >= 0 && code[j] == '\\'; j--)
+    {
+        backslashCount++;
+    }
+    return (backslashCount % 2 != 0);
 }

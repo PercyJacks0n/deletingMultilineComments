e5c164486a553d4f8927542aac55e734e85f9db4
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-14 01:54:34 +0300

    feat: implement findingEndMultiLineComment - green

--- findingEndMultiLineComment.cpp
+++ findingEndMultiLineComment.cpp
@@ -4,5 +4,22 @@
 
 void findingEndMultiLineComment(const QString & inputCode, int & i, LocationFlagInCode & state)
 {
+    int len = inputCode.length();
+    if (i + 1 >= len)
+    {
+        i++;
+        return;
+    }
 
+    QChar ch = inputCode[i];
+
+    if (ch == '*' && inputCode[i+1] == '/')
+    {
+        state = flagOuterCode;
+        i += 2;
+    }
+    else
+    {
+        i++;
+    }
 }

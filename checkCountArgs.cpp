031fbbe4e5a0046efcd4a9ff7a3178ce5b0ee747
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-13 16:36:18 +0300

    feat: implement checkCountArgs - green

--- checkCountArgs.cpp
+++ checkCountArgs.cpp
@@ -5,5 +5,12 @@
 
 void checkCountArgs(int argc, QList<Error> & errors)
 {
-
+    if (argc != 3)
+    {
+        Error error;
+        error.type = invalidArgumentsCount;
+        error.actualCount = argc - 1;
+        error.expectedValue = "2";
+        errors.append(error);
+    }
 }

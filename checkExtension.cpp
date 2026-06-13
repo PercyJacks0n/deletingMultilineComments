d806b711703b279b935070149f518304775e1f6d
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-14 01:16:09 +0300

    feat: implement checkExtension - green

--- checkExtension.cpp
+++ checkExtension.cpp
@@ -7,4 +7,20 @@
 void checkExtension(const QString & filePath, QList<QString> & allowedExtensions, QList<Error> & errors)
 {
 
+    QFileInfo fileInfo(filePath);
+    QString currentExtension = fileInfo.suffix();
+
+    if (!allowedExtensions.contains(currentExtension))
+    {
+        Error error;
+        error.type = invalidFileExtension;
+        error.errorInputFileWay = filePath;
+        error.actualValue = currentExtension;
+        QStringList extList;
+        for (const QString & ext : allowedExtensions) {
+            extList << ext;
+        }
+        error.expectedValue = extList.join(", ");
+        errors.append(error);
+    }
 }

e60fdc10d870f51cba332ac12fd247dc0d7eda20
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-14 01:28:03 +0300

    feat: implement checkPresenceOfDirective - green

--- checkPresenceOfDirective.cpp
+++ checkPresenceOfDirective.cpp
@@ -3,5 +3,23 @@
 
 void checkPresenceOfDirective(const QString & inputCode, int & i, int & countDirective, QString & directive)
 {
+    if (i + 1 >= inputCode.length()) return;
 
+    QString intendedDirective = "";
+    int pos = i + 1;
+
+    while (pos < inputCode.length() && (inputCode[pos] == ' ' || inputCode[pos] == '\t'))
+    {
+        pos++;
+    }
+
+    while (pos < inputCode.length())
+    {
+        QChar ch = inputCode[pos];
+        if (ch == ' ' || ch == '\t' || ch == '\n') break;
+        intendedDirective += ch;
+        pos++;
+    }
+
+    if (intendedDirective == directive) countDirective++;
 }

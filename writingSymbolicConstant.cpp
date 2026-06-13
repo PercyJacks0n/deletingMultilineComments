457e596cadaf863139110dcf9154f95426fd1e56
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-14 01:47:21 +0300

    feat: implement writingSymbolicConstant - green

--- writingSymbolicConstant.cpp
+++ writingSymbolicConstant.cpp
@@ -2,8 +2,20 @@
 #include "Functions.h"
 #include "LocationFlagInCode.h"
 
-void writingSymbolicConstant(const QString & inputCode, int & i, QString & result, LocationFlagInCode & state)
+void writingSymbolicConstant(const QString & inputCode, int & i, QString & result,LocationFlagInCode & state)
 {
+    if (i >= inputCode.length()) return;
 
+    QChar ch = inputCode[i];
+    result += ch;
+
+    if (ch == '\'')
+    {
+        if (!isQuoteEscaped(inputCode, i))
+        {
+            state = flagOuterCode;
+        }
+    }
+    i++;
 }
 

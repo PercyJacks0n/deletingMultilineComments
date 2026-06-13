0515e092e510af3c03252c6f5e417b72e3e432b0
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-14 01:58:49 +0300

    feat: implement findingEnteringOtherStatusFlags - green

--- findingEnteringOtherStatusFlags.cpp
+++ findingEnteringOtherStatusFlags.cpp
@@ -9,5 +9,42 @@ void findingEnteringOtherStatusFlags(const QString & inputCode,
                                      int & countDirective,
                                      QString & directive)
 {
+    int len = inputCode.length();
+    if (i >= len) return;
 
+    QChar ch = inputCode[i];
+
+    if(ch == '#')
+    {
+        checkPresenceOfDirective(inputCode, i, countDirective, directive);
+    }
+
+    if (i + 1 < len && ch == '/' && inputCode[i+1] == '*') {
+        state = flagMultiLineComment;
+        i += 2;
+    }
+
+    else if (i + 1 < len && ch == '/' && inputCode[i+1] == '/') {
+        result += ch;
+        result += inputCode[i+1];
+        state = flagOneLineComment;
+        i += 2;
+    }
+
+    else if (ch == '"') {
+        result += ch;
+        state = flagStringConstant;
+        i++;
+    }
+
+    else if (ch == '\'') {
+        result += ch;
+        state = flagSymbolicConstant;
+        i++;
+    }
+
+    else {
+        result += ch;
+        i++;
+    }
 }

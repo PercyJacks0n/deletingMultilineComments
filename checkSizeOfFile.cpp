5399d312035f56bfcec1eb5a1dcb16e940e2bd58
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-14 01:23:26 +0300

    feat: implement checkSizeOfFile - green

--- checkSizeOfFile.cpp
+++ checkSizeOfFile.cpp
@@ -6,5 +6,30 @@
 
 void checkSizeOfFile(const QString & inputCode, QList<Error> & errors)
 {
+    QStringList lines = inputCode.split('\n');
 
+    if (lines.size() > 1000)
+    {
+        Error error;
+        error.type = numberRowsExceeded;
+        error.maxAllowed = 1000;
+        error.actualCount = lines.size();
+        errors.append(error);
+    }
+
+    QList<int> longLines;
+    for (int i = 0; i < lines.size(); ++i)
+    {
+        if (lines[i].length() > 1000)
+            longLines.append(i + 1);
+    }
+
+    if (!longLines.isEmpty())
+    {
+        Error error;
+        error.type = numberCharactersPerLineExceeded;
+        error.maxAllowed = 1000;
+        error.lineNumbers = longLines;
+        errors.append(error);
+    }
 }

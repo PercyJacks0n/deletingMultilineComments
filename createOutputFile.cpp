5daf30839fce633e8a729c8dfd2686cea06db534
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-14 01:30:30 +0300

    feat: implement createOutputFile - green

--- createOutputFile.cpp
+++ createOutputFile.cpp
@@ -6,7 +6,23 @@
 #include "ErrorType.h"
 #include <QFile>
 
-void createOutputFile(const QString & content, const QString & filePath,QList<Error> & errors)
+void createOutputFile(const QString & content,
+                      const QString & filePath,
+                      QList<Error> & errors)
 {
+    QFile file(filePath);
 
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        Error error;
+        error.type = outputFileCreateError;
+        error.errorOutputFileWay = filePath;
+        errors.append(error);
+        return;
+    }
+
+    QTextStream stream(&file);
+    stream << content;
+
+    file.close();
 }

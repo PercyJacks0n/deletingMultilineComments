41f1d2faab8461c43aeb098c00c25f70f66a01bd
Ежов Д.Д <yezhov.06@inbox.ru>
2026-06-15 19:29:02 +0300

    Add accidentally deleted closing bracket has been added in findingEnteringOtherStatusFlags.cpp

--- findingEnteringOtherStatusFlags.cpp
+++ findingEnteringOtherStatusFlags.cpp
@@ -22,32 +22,37 @@ void findingEnteringOtherStatusFlags(const QString & inputCode,
     }
 
     // Начало многострочного комментария /*
-    if (i + 1 < len && ch == '/' && inputCode[i+1] == '*') {
+    if (i + 1 < len && ch == '/' && inputCode[i+1] == '*')
+    {
         state = flagMultiLineComment;   // переключаем состояние
         i += 2;                         // пропускаем /*
-
-        // Начало однострочного комментария //
-        else if (i + 1 < len && ch == '/' && inputCode[i+1] == '/') {
-            result += ch;                   // добавляем первый слеш
-            result += inputCode[i+1];       // добавляем второй слеш
-            state = flagOneLineComment;     // переключаем состояние
-            i += 2;
-
-            // Начало строковой константы "
-            else if (ch == '"') {
-                result += ch;
-                state = flagStringConstant;
-                i++;
-
-                // Начало символьной константы '
-                else if (ch == '\'') {
-                    result += ch;
-                    state = flagSymbolicConstant;
-                    i++;
-
-                    // Обычный символ то просто копируем
-                    else {
-                        result += ch;
-                        i++;
-                    }
-                }
+    }
+    // Начало однострочного комментария //
+    else if (i + 1 < len && ch == '/' && inputCode[i+1] == '/')
+    {
+        result += ch;                   // добавляем первый слеш
+        result += inputCode[i+1];       // добавляем второй слеш
+        state = flagOneLineComment;     // переключаем состояние
+        i += 2;
+    }
+    // Начало строковой константы "
+    else if (ch == '"')
+    {
+        result += ch;
+        state = flagStringConstant;
+        i++;
+    }
+    // Начало символьной константы '
+    else if (ch == '\'')
+    {
+        result += ch;
+        state = flagSymbolicConstant;
+        i++;
+    }
+    // Обычный символ то просто копируем
+    else
+    {
+       result += ch;
+       i++;
+    }
+}
